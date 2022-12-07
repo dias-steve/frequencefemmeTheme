521da7bdb8ac99ca2ad03848743570eae537f4bc
Steve Dias <stevediaspro@gmail.com>
2022-12-07 16:33:56 +0100

    [FIX][CONVERT BLOC] done

--- utils/frequencefemmetheme-utils.php
+++ utils/frequencefemmetheme-utils.php
@@ -1,9 +1,11 @@
 <?php
 
     function convert_content_list ($content_list) {
-        for ($i = 0; $i < count($content_list); $i++) {
-            if ($content_list[$i]["bloc_type"] == "path"){
-                $content_list[$i]["step_list"] = get_all_Steps(); // steps-utils
+        if(is_array($content_list)){
+            for ($i = 0; $i < count($content_list); $i++) {
+                if ($content_list[$i]["bloc_type"] == "path"){
+                    $content_list[$i]["step_list"] = get_all_Steps(); // steps-utils
+                }
             }
         }
     
