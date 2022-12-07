289d27b103a19d8f6bd4498d382c3635cb59c619
Steve Dias <stevediaspro@gmail.com>
2022-12-07 11:50:17 +0100

    [ADD][Apropos page API]

--- components/step/step-api.php
+++ components/step/step-api.php
@@ -1,50 +1,41 @@
 <?php
 
-function actionRoute() {
-    register_rest_route('io/v1','actions/(?P<id>\d+)',array(
+function stepRoute() {
+    register_rest_route('io/v1','steps/(?P<id>\d+)',array(
         'methods' => WP_REST_Server::READABLE,
-        'callback' => 'getActionDetail' // notre fonfonction 
+        'callback' => 'getstepDetail' // notre fonfonction 
     ));
 
-    register_rest_route('io/v1','actioncats/(?P<categoriesfilter>\d+)',array(
+
+
+    register_rest_route('io/v1','steps',array(
         'methods' => WP_REST_Server::READABLE,
-        'callback' => 'getActionCatDetail' // notre fonfonction 
+        'callback' => 'getAllsteps' // notre fonfonction 
     ));
 
-    register_rest_route('io/v1','actions',array(
+
+    register_rest_route('io/v1','stepspage',array(
         'methods' => WP_REST_Server::READABLE,
-        'callback' => 'getAllActions' // notre fonfonction 
+        'callback' => 'getStepPage' // notre fonfonction 
     ));
 
-    register_rest_route('io/v1','actions',array(
-        'methods' => 'POST',
-        'callback' => 'getActionCatDetail' // notre fonfonction 
-    ));
+    
 
-    register_rest_route('io/v1','actioncats',array(
-        'methods' => WP_REST_Server::READABLE,
-        'callback' => 'getAllActionsCat' // notre fonfonction 
-    ));
+
 }
-add_action('rest_api_init', 'actionRoute');
+add_action('rest_api_init', 'stepRoute');
 
-function getActionDetail($data){
-    return getActionById($data['id']);
-}
-
-function getActionCatDetail($data){
-    return getActionByCat($data['categoriesfilter'], $data['page'],8);
-}
-
-function getAllActions(){
-    return get_all_actions();
-}
-
-function getAllActionsCat(){
-    return get_all_actionCat();
+function getstepDetail($data){
+    return getstepById($data['id']);
 }
 
 
-function getAllActionsPaged($data){
-    return 'ok';
+function getAllsteps(){
+    return get_all_steps();
 }
+
+function getStepPage(){
+    return get_step_page_data();
+}
+ 
+
--- components/step/step-utils.php
+++ components/step/step-utils.php
@@ -1,12 +1,12 @@
 <?php //
 
-function getActionById($id){
+function getStepById($id){
 
     $results = null;
     
     $mainQuery = new WP_Query(array(
         'posts_per_page' => -1,
-        'post_type' => array('actions'),
+        'post_type' => array('step'),
         'post_status' => 'publish',
         'post_id' => $id,
         
@@ -17,95 +17,70 @@ function getActionById($id){
 
 
         if(get_the_ID() == $id){
-            $results = get_Action_detail_formated();
+            $results = get_Step_detail_formated();
         }
     }
     return $results;    
 }
 
 
-function getActionByCat($idCat, $nbPage ,$nbPost){
-    if($idCat){
-        $results = array(
-            'page' => array( 
-                'current_page' => $nbPage,
-                'page_nb_max' => 10
-            ),
-            'list_post' => array()
-        );
-        $mainQuery = new WP_Query(array(
-            'posts_per_page' => $nbPost,
-            'post_type' => array('actions'),
-            'post_status' => 'publish',
-            'paged'=> $nbPage,
-            'tax_query' => array(
-                array(
-                    'taxonomy' => 'action_cat',
-                    'field'    => 'term_id',
-                    'terms'    => $idCat,
-                    
-                ),
-            ),
-        ));
 
-        while($mainQuery->have_posts()) {
 
-            $results['page'] = array(
-                'current_page' => $nbPage,
-                'page_nb_max' => $mainQuery->max_num_pages,
-                'nb_posts_found' => $mainQuery->found_posts
-            );
-
-            $mainQuery->the_post();
-            array_push($results['list_post'],  get_Action_detail_formated());
-            
-        }
-        return $results; 
-    }else{
-        $results = array(
-            'page' => array( 
-                'current_page' => 1,
-                'page_nb_max' => 1
-            ),
-            'list_post' => array()
-        );
-        
-        return $results;  
-    }
-}
-
-function get_all_actions(){
+function get_all_Steps(){
     $results= array();
     $mainQuery = new WP_Query(array(
         'posts_per_page' => -1,
-        'post_type' => array('actions'),
+        'post_type' => array('step'),
         'post_status' => 'publish',
 
     ));
 
     while($mainQuery->have_posts()) {
         $mainQuery->the_post();
-        array_push($results, get_Action_detail_formated());
+        array_push($results, get_Step_detail_formated());
         
     }
     return $results;   
 }
 
-function get_all_actionCat(){
-    return get_all_terms_list_reformat( 'action_cat', '/actioncat' );
-}
 
-function get_Action_detail_formated(){
+
+function get_Step_detail_formated(){
     return array ('id' => get_the_ID(),
     'title' => get_the_title(),
     'thumbnail' => getThumbnailFormated(get_the_ID()),
-    'author' => get_the_author(),
     'content' => get_the_content(),
-    'taxinomie' =>  get_term_list_reformat(get_the_ID(),'action_cat', '/actioncat'),
-    'tag_list' =>  get_term_list_reformat(get_the_ID(),'post_tag','/tag'),
-    'post_type' => get_post_type(),
-    'link' => get_the_post_link_front(),
-    'seo' => get_seo_data(),
+    'step_number' => get_field('step_number')
 );
+}
+
+function get_step_page_data(){
+    $results = null;
+    
+    $mainQuery = new WP_Query(array(
+        'posts_per_page' => -1,
+        'post_type' => array('page'),
+        'post_status' => 'publish',
+        'pagename' => 'parcours'
+        
+    ));
+
+    while($mainQuery->have_posts()) {
+        $mainQuery->the_post();
+
+        $results= get_step_page_data_formated();
+        
+    }
+    return $results;    
+}
+
+function get_step_page_data_formated(){
+    return array(
+        'id' => get_the_ID(),
+        'title' => get_field('title_page'),
+        'seo' => get_seo_data(),
+        'content' => get_field('content_list')
+
+    );
 }
 ?>
