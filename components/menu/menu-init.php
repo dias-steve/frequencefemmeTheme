61b44967c8e1f556aa91111602054f2530991765
Steve Dias <stevediaspro@gmail.com>
2022-12-22 15:34:29 +0100

    step fixing

--- components/menu/menu-init.php
+++ components/menu/menu-init.php
@@ -4,7 +4,8 @@
         'footer_aide_sec' => __( 'Menu Footer section aide' ),
         'footer_legal_sec' => __( 'Menu Footer section légale' ),
         'footer_social_sec' => __( 'Menu Footer section sociale'),
-        'menu_header_sec' => __('Menu Header')
+        'menu_header_sec' => __('Menu Header'),
+        'menu_header_orange' => __('Btn orange (en Haut à droite)')
        )
      );
    }
